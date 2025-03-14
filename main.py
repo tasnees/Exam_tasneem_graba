53c948368bb963ee8a026e30f1252a36e3316477
tasnees <graba.tasneem@gmail.com>
2025-03-14 09:38:55 +0100

    latest version of main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+my_list=[1,5,7]
+if my_list:
    print('la somme est:',sum(my_list))
    print('la somme est:',max(my_list))
    print('la somme est:',min(my_list))
-
+else:
+   print('liste vide')
 
 
 
