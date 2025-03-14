d78444f6737f6d0175eac1ce2ca5fc942534cbe8
tasnees <graba.tasneem@gmail.com>
2025-03-14 09:29:10 +0100

    final version

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+def somme(L):
+    s=0
+    for e in L:
+        s+=e
+    return s
+
+
 my_list=[1,5,7]
-print('la somme est:',sum(my_list))
+som=somme(my_list)
+print('la somme est =',som)
+
+my_list=[1,5,7]
+print('la somme est =',sum(my_list))
+
+
 
 
 
