a094c98c3e0da08575199fc41befced83a33b853
tasnees <graba.tasneem@gmail.com>
2025-03-14 09:35:04 +0100

    new version of main.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-def somme(L):
-    s=0
-    for e in L:
-        s+=e
-    return s
-
-
 my_list=[1,5,7]
-som=somme(my_list)
-print('la somme est =',som)
-
-my_list=[1,5,7]
-print('la somme est =',sum(my_list))
-
+if my_list:
+   print('la somme est:',sum(my_list))
+   print('la somme est:',max(my_list))
+   print('la somme est:',min(my_list))
+else:
+   print('liste vide')
 
 
 
